fd766747d319839d759ad81f218e0655d073c81e
lazyDev0811 <dev.sun0811@gmail.com>
2019-01-07 17:26:05 +0100

    add  better custom exception message

--- backend/src/main/java/szbd/projekt/projektbazy/genre/GenreController.java
+++ backend/src/main/java/szbd/projekt/projektbazy/genre/GenreController.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class GenreController {
@@ -36,7 +37,11 @@ public class GenreController {
 	@RequestMapping(method=RequestMethod.DELETE,value="/genre/{genreName}")
 	public void deleteGenre(@PathVariable String genreName) {
 
-		genreService.deleteGenre(genreName);
+		try {
+			genreService.deleteGenre(genreName);
+		} catch (Exception ex) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Element is foreign key", ex);
+		}
 	}
 	
 	@RequestMapping(method=RequestMethod.PUT,value="/genre/{genreName}")
@@ -44,5 +49,6 @@ public class GenreController {
 	{
 		 genreService.updateGenre(genreName, genre);
 	}
+
 	
 }
